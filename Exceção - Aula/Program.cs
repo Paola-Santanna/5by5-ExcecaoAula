5c28b0b1242d2e83b07b88fb318dd55e2feeb38d
Paola-Santanna <paolinhasj@gmail.com>
2024-05-08 09:49:29 -0300

    #Continuação
    Continuação da aula.

--- Exceção - Aula/Program.cs
+++ Exceção - Aula/Program.cs
@@ -16,7 +16,28 @@ internal class Program
         catch (Exception e) // O "e" é o objeto que será retornado como tratamento da exceção
         {
             Console.WriteLine("Simpatia, informe um número inteiro!");
+            Console.WriteLine("Mensagem retornada da exceção: " + e.ToString());
+            Console.WriteLine("\nDados da exceção" + e.Source.GetType());
+
+        }
+        finally
+        {
+            /* 
+             * Se deu erro ou não, o "finally" é executado
+             * Ele é de uso opcional
+             * É mais para saber que "você chegou no final do bloco"
+             * A principal funcão dele é controlar todo o bloco
+             */
+            Console.WriteLine("\nFinal do tratamento da exceção");
         }
         Console.WriteLine($"O valor obtido foi {num}");
+
+        /*
+         * O try é usado como controle para que o usuário não faça algo ruim à execução do código
+         * 
+         * A ideia do log é o registro de cada processo ocorrido, inclusive os erros
+         * 
+         * 
+         */
     }
 }
