ed59b54355dcbbcfbdb6ed966338c5231f199529
Paola-Santanna <paolinhasj@gmail.com>
2024-05-08 09:27:56 -0300

    Add project files.

---
+++ Exceção - Aula/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 0;
+
+        Console.WriteLine("Insira um número inteiro: ");
+        try
+        {
+            num = int.Parse(Console.ReadLine());
+        }
+        catch (Exception e) // O "e" é o objeto que será retornado como tratamento da exceção
+        {
+            Console.WriteLine("Simpatia, informe um número inteiro!");
+        }
+        Console.WriteLine($"O valor obtido foi {num}");
+    }
+}
